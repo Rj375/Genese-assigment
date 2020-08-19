89b2f835862f7b3c6ad8de3e337141c2b02219a7
Ramesh Giri <reyluca357@gmail.com>
2020-08-19 14:28:57 +0545

    added calculate and output logic in areas

--- src/components/Area.vue
+++ src/components/Area.vue
@@ -12,7 +12,7 @@ export default {
             document.getElementById("input-rectangle").hidden = true;
             document.getElementById("input-square").hidden = true;
 
-        // get the selected shape and show its input
+        // get the selected shape and show its input using switch statement
             let shape = document.getElementById("shapes").value;
             switch (shape) {
                 case "circle":
@@ -30,7 +30,43 @@ export default {
             }
         },
 
-        // video ko 12:00 paxi herne
+        calculateArea(){
+            //get the selected shape
+             let shape = document.getElementById("shapes").value;
+
+            //calculate the shape's area using switch statement
+            let area = 0;
+            switch (shape){
+                case "circle":{  //yo {} use vaxa yesma, coz eslint le vanxa let , const variables use garda {} vitra lekhne 
+                                 //(eg:  Unexpected lexical declaration in case block  no-case-declarations) natra yesto error aunxa
+                    let Radius = document.getElementById("radius").value;
+                    area = Math.PI * Radius * Radius;
+                    break;
+                }
+                case "triangle":{    //yo {} use vaxa yesma, coz eslint le vanxa let , const variables use garda {} vitra lekhne 
+                                    //(eg:  Unexpected lexical declaration in case block  no-case-declarations) natra yesto error aunxa
+                    let Base = document.getElementById("base").value;
+                    let Height = document.getElementById("height").value;
+                    area =  Base * Height / 2;
+                    break;
+                }
+                case "rectangle":{    //yo {} use vaxa yesma, coz eslint le vanxa let , const variables use garda {} vitra lekhne 
+                                     //(eg:  Unexpected lexical declaration in case block  no-case-declarations) natra yesto error aunxa
+                    let Length = document.getElementById("length").value;
+                    let Width = document.getElementById("width").value;
+                    area = Length * Width;
+                    break;
+                }
+                case "square":{     //yo {} use vaxa yesma, coz eslint le vanxa let , const variables use garda {} vitra lekhne 
+                                   //(eg:  Unexpected lexical declaration in case block  no-case-declarations) natra yesto error aunxa
+                    let Side = document.getElementById("side").value;
+                    area =  Side * Side;
+                    break;
+                }
+            }
+                 //output the area
+              document.getElementById("output").innerHTML = "Area = " + area;
+        }
     }
 
 
